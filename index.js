db49518eb4aded8b783f54938c1107e05b997657
Pratham Sharma <1pratham1sharma@gmail.com>
2024-12-25 08:56:31 +0530

    mongoConnection

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from './utils/db.js';
+dotenv.config({});
 
 
 const app = express();
@@ -20,9 +23,10 @@ app.use(cors(corsOptions))
 
 
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 
 app.listen(PORT,()=>{
+    connectDB();
      console.log(`server is running on http://localhost:${PORT}`)
 })
