d40b9295e67327123380ed51c408484e43e201f6
Pratham Sharma <1pratham1sharma@gmail.com>
2024-12-26 14:13:43 +0530

    rcv of company and user

---
+++ controllers/company.controller.js
@@ -0,0 +1,113 @@
+import {Company} from '../models/company.model.js';
+export const registercompany = async(req,res)=>{
+
+    const {companyName} = req.body;
+    //console.log(companyName);
+    if(!companyName){
+        return res.status(400).json({
+            message:"company name is required",
+            success:"false"
+
+        })
+    }
+    let company = await Company.findOne({name:companyName})
+    // console.log(company);
+    if(company){
+        return res.status(400).json({
+            message:"company name is exist choose another name",
+            success:"false"
+
+        })
+    }
+    company = await Company.create({
+        name:companyName,
+        userId:req.id //from cookie
+
+    });
+    return res.status(201).json({
+        message:"company created",
+        company,
+        success:"true "
+
+    })
+}
+
+
+export const getCompany = async(req,res)=>{
+       try {
+        const userId = req.id    //loggedinUser
+        const company = await Company.find({userId});
+        if(!company){
+            return res.status(400).json({
+                message:"no company found",
+                success:"false"
+        
+            })
+        }
+        else{
+            return res.status(200).json({
+                message:"company found",
+                company,
+                success:"true"
+        
+            })
+        }
+       } catch (error) {
+        console.log(error)
+       }
+}
+
+
+export const getCompanyById = async(req,res)=>{
+    try {
+        const companyId = req.params.id 
+        //console.log(companyId);    
+        const company = await Company.findById(companyId);
+        if(!company){
+            return res.status(400).json({
+                message:"no company found",
+                success:"false"
+        
+            })
+        }
+        else{
+            return res.status(200).json({
+                message:"company found",
+                company,
+                success:"true"
+        
+            })
+        }
+       } catch (error) {
+        console.log(error)
+       }
+}
+
+
+
+export const updateCompany = async(req,res)=>{
+    try {
+        const {name,description,website,location} = req.body;
+        const file = req.file;
+
+        const updateData = {name,description,website,location};
+        const company = await Company.findByIdAndUpdate(req.params.id,updateData,{new:true});
+        
+        if(!company){
+            return res.status(400).json({
+                message:"no company found",
+                success:"false"
+        
+            })
+        }
+        return res.status(200).json({
+            message:"company information updated",
+            success:"true"
+    
+        })
+        
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
---
+++ controllers/user.controller.js
@@ -0,0 +1,156 @@
+import { User } from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
+import jwt from "jsonwebtoken";
+
+export const register = async(req,res)=>{
+       try {
+         const {fullname,email,phoneNumber,password,role} = req.body;
+         if(!fullname || !email || !phoneNumber || !password ||!role){
+            return res.status(400).json({
+                message:'All Fields are required',
+                success:false
+            });
+         }
+         const user = User.findOne({email});
+         if(!user){
+            return res.status(400).json({
+                message:'email already taken',
+                success:true
+            })
+         }
+         const hashedPassword = await bcrypt.hash(password,10)
+         await User.create({
+            fullname,
+            email,
+            phoneNumber,
+            password:hashedPassword,
+            role,
+         });
+         return res.status(201).json({
+            message:"registered successfully",
+            success:true
+         })
+       } catch (error) {
+         console.log(error)
+       }
+}
+
+export const login = async(req,res)=>{
+       try {
+        const {email,password,role} = req.body;
+        if(!email || !password ||!role){
+            return res.status(400).json({
+                message:"email and password and role is required",
+                success:false
+            })
+        }
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({
+                message:"cannot find email",
+                success:false
+            })
+        }
+        const isPasswordMatch = await bcrypt.compare(password,user.password)
+        if(!isPasswordMatch){
+            return res.status(400).json({
+                message:"password doesn't match",
+                success:false
+            })
+        }
+        if(role !== user.role){
+            return res.status(400).json({
+                message:"role doesn't match",
+                success:false
+            })
+        }
+        const tokenData = {
+            userId:user._id
+        }
+        const token = await jwt.sign(tokenData,process.env.SECRET_KEY,{expiresIn:'1d'});
+        user = {
+            _id:user._id,
+            fullname:user.fullname,
+            role:user.role,
+        }
+        return res.status(200).cookie("token",token,{maxAge:1*24*60*60*1000,httpsOnly:true,sameSite:'strict'}).json({
+            message:`welcome back ${user.fullname}`,
+            user,
+            success:true
+        })
+
+       } catch (error) {
+         console.log(error)
+       }
+    }
+
+export const logout = async(req,res)=>{
+    try {
+        return res.status(200).cookie("token","",{maxAge:0}).json({
+            message:"logout successfully",
+            success:true
+        })
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const updateProfile = async(req,res)=>{
+    try {
+        const {fullname,email,phoneNumber,bio,skills} = req.body;
+        const file = req.file;
+        
+         //cloudinary
+
+        let skillsArray;
+        if(skillsArray){
+            const skillsArray = skills.split(",");
+        }
+
+         
+         const userId = req.id; // token se
+         let user = await User.findById(userId);
+         if(!user){
+            return res.status(400).json({
+                message:"cannot find user",
+                success:false
+            })
+        }
+        //updating data
+        if(fullname) user.fullname = fullname
+        if(email) user.email = email
+        if(phoneNumber) user.phoneNumber = phoneNumber
+        if(bio) user.bio = bio
+        if(skills) user.skills = skillsArray
+        //resume
+        await user.save();
+        user = {
+            _id : user._id,
+            fullname:user.fullname,
+            email:user.email,
+            phoneNumber:user.phoneNumber,
+            role:user.role,
+            profile : user.profile,
+
+
+        }
+        return res.status(200).json({
+            message:"user updated successfully",
+            user,
+            success:true
+        })
+
+
+
+
+
+
+    } catch (error) {
+        console.log(error)
+    }
+    
+
+}
+
+
